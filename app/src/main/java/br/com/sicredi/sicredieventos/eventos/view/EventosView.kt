cd0e426867f25fbf2267b9abb9e6774c848217e0
KryslanGomes <kryslan.gomes-partner@aperam.com>
2022-02-11 23:54:51 -0300

    add. toolbar.xml

--- app/src/main/java/br/com/sicredi/sicredieventos/eventos/view/EventosView.kt
+++ app/src/main/java/br/com/sicredi/sicredieventos/eventos/view/EventosView.kt
@@ -3,27 +3,51 @@ package br.com.sicredi.sicredieventos.eventos.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.widget.Toolbar
 import br.com.sicredi.sicredieventos.R
 import br.com.sicredi.sicredieventos.eventos.presenter.EventosPresenter
 
 class EventosView : AppCompatActivity() {
 
+    //region Globais
     var presenter = EventosPresenter(this)
+    //endregion Globais
 
+    //region onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eventos)
 
+        configuraToolbar()
+
         buscaEventos()
 
     }
+    //endregion onCreate
 
+    //region Eventos
+    /**
+     * Busca eventos no servidor.
+     * Retorno em {@link #recebeEventos}
+     */
     private fun buscaEventos(){
 
         presenter.model.buscaEventos()
-    }
 
+    }
+    /**
+     * Exibe os eventos.
+     * Solicitado em {@link #buscaEventos}
+     */
     fun recebeEventos(valor: Int) {
         Toast.makeText(this, "EVENTOS RECEBIDOS" + valor, Toast.LENGTH_SHORT).show()
     }
+    //endregion Eventos
+
+    //region Utilitarios
+    private fun configuraToolbar() {
+        supportActionBar?.title = "Eventos"
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+    //endregion Utilitarios
 }
