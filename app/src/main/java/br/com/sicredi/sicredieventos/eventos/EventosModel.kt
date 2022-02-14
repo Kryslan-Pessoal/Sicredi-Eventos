4298382daaba8584ba72077e3c688d9f554102b3
KryslanGomes <kryslan.gomes-partner@aperam.com>
2022-02-14 12:12:02 -0300

    Add. SolicitacaoGet/ SolicitacaoPost.java

--- app/src/main/java/br/com/sicredi/sicredieventos/eventos/EventosModel.kt
+++ app/src/main/java/br/com/sicredi/sicredieventos/eventos/EventosModel.kt
@@ -1,10 +1,39 @@
 package br.com.sicredi.sicredieventos.eventos
 
+import br.com.sicredi.sicredieventos.utilitarios.Constantes
+import br.com.sicredi.sicredieventos.utilitarios.LoadedV2.LoadedV2
+import br.com.sicredi.sicredieventos.utilitarios.LoadedV2.SolicitacaoGet
+import java.lang.Exception
+
 class EventosModel (eventosPresenter: EventosPresenter) {
 
     private val presenter = eventosPresenter
 
     fun buscaEventos() {
+
+        val url = Constantes.API_GET_EVENTOS + "events"
+
+        val solicitacaoGet = SolicitacaoGet()
+
+        val impl = object : LoadedV2 {
+            /** Quando a resposta do Servidor é bem sucedida
+             * @param retorno: Retorno do servidor (convertido para String)
+             */
+            override fun onResponse(retorno: String?) {
+                var eu = 22;
+            }
+
+            /** Quando ocorre algum erro na comunicação
+             * @param retorno: Erro retornado
+             * @param throwable: Exception lançada (caso tenha) (pode ser null se não tiver)
+             */
+            override fun onFailure(retorno: String?, throwable: Exception?) {
+                var eu = 22;
+            }
+        }
+
+        solicitacaoGet.send(url, impl)
+
         presenter.processaBuscaEventos()
     }
 
---
+++ app/src/main/java/br/com/sicredi/sicredieventos/utilitarios/Constantes.kt
@@ -0,0 +1,7 @@
+package br.com.sicredi.sicredieventos.utilitarios
+
+public class Constantes {
+    companion object{
+        const val API_GET_EVENTOS = "https://5f5a8f24d44d640016169133.mockapi.io/api/"
+    }
+}
